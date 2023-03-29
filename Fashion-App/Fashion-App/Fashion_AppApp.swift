a249fc7377ef767bb5d8bd4175a6f9ab94f61362
Alexandra Rivera <alexandrarivera224@gmail.com>
2023-03-29 10:31:33 -0400

    initial Xcode commit

---
+++ Fashion-App/Fashion-App/Fashion_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fashion_AppApp.swift
+//  Fashion-App
+//
+//  Created by Alexandra Rivera on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct Fashion_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
