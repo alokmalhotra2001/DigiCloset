bd0fa0b9468118e40b32928e2bd5da51a7ec3b47
Deeya Garg <deeya@Deeyas-MacBook-Air.local>
2023-04-05 19:07:11 -0400

    reduced date picker only to next week

--- Fashion-App/Fashion-App/Features/Calendar/Calendar.swift
+++ Fashion-App/Fashion-App/Features/Calendar/Calendar.swift
@@ -3,16 +3,23 @@ import SwiftUI
 struct Calendar: View {
     
     @State private var date = Date()
-    //let dateRange:
+    
+    var dateClosedRange: ClosedRange<Date> {
+        let min = Date()
+        let max = Date.now.addingTimeInterval(60*60*24*7)
+        return min...max
+    }
     
     var body: some View {
         
         NavigationStack {
             ScrollView {
-                DatePicker("Selected Date", selection: $date, in: Date()..., displayedComponents: .date)
+                DatePicker("Selected Date", selection: $date,
+                           in: dateClosedRange,
+                           displayedComponents: .date)
                     .datePickerStyle(GraphicalDatePickerStyle())
             }
-//            //.navigationTitle("Calendar View")
+            //.navigationTitle(date)
         }
     }
 }
@@ -20,23 +27,5 @@ struct Calendar: View {
 struct Calendar_Previews: PreviewProvider {
     static var previews: some View {
         Calendar()
-    }
-}
-
-struct CalendarView: UIViewRepresentable {
-    let interval: DateInterval
-    
-    func makeUIView(context: Context) -> UICalendarView {
-        let calView = UICalendarView()
-        calView.backgroundColor = .white
-        calView.availableDateRange = interval
-        calView.calendar = .current
-        calView.locale = .current
-        calView.fontDesign = .rounded
-        return calView
-    }
-    
-    func updateUIView(_ uiView: UICalendarView, context: Context) {
-        
     }
 }
